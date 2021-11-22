559b6e92911bb63978a570fd9444d8334080f27a
Diaz1401 <reagor8161@outlook.com>
2021-11-22 16:41:27 +0700

    Import x86_64 GCC 20211122
    Build completed on: Mon Nov 22 16:41:27 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/x86_64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/x86/bin/../libexec/gcc/x86_64-elf/9.4.1/lto-wrapper
    Target: x86_64-elf
    Configured with: ../gcc/configure --target=x86_64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../x86 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion=Cat•GCC --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    gcc version 9.4.1 20211122 (Cat•GCC)
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- lib/gcc/x86_64-elf/9.4.1/plugin/include/configargs.h
+++ lib/gcc/x86_64-elf/9.4.1/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=x86_64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../x86 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion=ChokiGCC --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char configuration_arguments[] = "../gcc/configure --target=x86_64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../x86 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion=Cat•GCC --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
 static const char thread_model[] = "single";
 
 static const struct {
