1b3a4c37c0781f177f6eb4bc839860e49437ff1d
Diaz1401 <reagor8161@outlook.com>
2022-02-05 10:34:49 +0700

    Import x86_64 GCC 20220205
    Build completed on: Sat Feb  5 10:34:49 WIB 2022
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/x86_64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/x86/bin/../libexec/gcc/x86_64-elf/12.0.1/lto-wrapper
    Target: x86_64-elf
    Configured with: ../gcc/configure --target=x86_64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../x86 --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion=CatGCC --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib
    gcc version 12.0.1 20220205 (experimental) (CatGCC)
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

---
+++ lib/gcc/x86_64-elf/12.0.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=x86_64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../x86 --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion=CatGCC --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
